5c55e738146945fd32fa1e1214d94f316cdfadec
Felipe <umpierre.felipe@gmail.com>
2015-03-18 00:23:04 -0300

    First commit

---
+++ src/Cliente.java
@@ -0,0 +1,65 @@
+/**
+ * Created by felipeumpierre on 18/03/15.
+ */
+public class Cliente {
+    private String name, cpf, rg, phone;
+    private ArrayList<Livro> livros = new ArrayList<Livro>();
+
+    public Cliente(String name, String cpf, String rg, String phone)
+    {
+        this.setName( name );
+        this.setCpf( cpf );
+        this.setRg( rg );
+        this.setPhone( phone );
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName( String name )
+    {
+        this.name = name;
+    }
+
+    public String getCpf()
+    {
+        return cpf;
+    }
+
+    public void setCpf( String cpf )
+    {
+        this.cpf = cpf;
+    }
+
+    public String getRg()
+    {
+        return rg;
+    }
+
+    public void setRg( String rg )
+    {
+        this.rg = rg;
+    }
+
+    public String getPhone()
+    {
+        return phone;
+    }
+
+    public void setPhone( String phone )
+    {
+        this.phone = phone;
+    }
+
+    public ArrayList<Livro> getLivros()
+    {
+        return livrosLivroArrayList;
+    }
+
+    public void setLivros( ArrayList<Livro> livrosLivroArrayList )
+    {
+        this.livrosLivroArrayList = livrosLivroArrayList;
+    }
+}

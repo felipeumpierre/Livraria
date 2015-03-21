4cc4b80e42f77575dcf9dae374f03fa204c3c77a
Felipe Pieretti Umpierre <felipeumpierre@hotmail.com>
2015-03-21 10:47:19 -0300

    Update main.java
    Adicionado comentários

--- src/main/main.java
+++ src/main/main.java
@@ -8,18 +8,31 @@ public class main
 {
     public static void main( String[] args )
     {
+        // create a new client
         Cliente cliente = new Cliente( "Felipe", "123456", "123456" );
+        
+        // create a new book
         Livro livro = new Livro( 1, "livro #1", "author", "publisher", "ABC", "1234AVC", "pt_BR", 2015, "Resume" );
+        
+        // create another book
         Livro livro2 = new Livro( 2, "livro #2", "author", "publisher", "ABC", "1234AVC", "pt_BR", 2015, "Resume" );
 
+        // insert the first book to the client
         cliente.addLivro( livro );
+        
+        // insert the other book to the client
         cliente.addLivro( livro2 );
 
+        // create the object Carrinho
         Carrinho carrinho = new Carrinho();
+        
+        // add the client to it
         carrinho.addCliente( cliente );
 
+        // now, to list the informations saved in the ArrayList
         ArrayList<Cliente> clienteArrayList = carrinho.getCompras();
 
+        // make a loop to print all the informations
         for( Cliente cli: clienteArrayList )
         {
             System.out.println( "Nome: " + cli.getName() );

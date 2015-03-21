9ab5c1abaa52d4965921ad47627c1364400e8fc0
Felipe Pieretti Umpierre <felipeumpierre@hotmail.com>
2015-03-21 10:47:48 -0300

    Update main.java

--- src/main/main.java
+++ src/main/main.java
@@ -1,9 +1,11 @@
 package main;
 
 import classe.*;
-
 import java.util.ArrayList;
 
+/**
+ * @author Felipe Pieretti Umpierre <umpierre.felipe@gmail.com>
+ */
 public class main
 {
     public static void main( String[] args )

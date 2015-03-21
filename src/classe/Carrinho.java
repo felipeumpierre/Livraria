65dff08912bc91fec8e3b22c2808800b6b25a6f0
Felipe Pieretti Umpierre <felipeumpierre@hotmail.com>
2015-03-21 10:39:37 -0300

    Update Carrinho.java
    Adicionado comentários e javadoc

--- src/classe/Carrinho.java
+++ src/classe/Carrinho.java
@@ -2,15 +2,33 @@ package classe;
 
 import java.util.ArrayList;
 
+/**
+ * @author Felipe Pieretti Umpierre <umpierre.felipe@gmail.com>
+ * 
+ * Classe para manipulação de clientes no carrinho de compra.
+ */
 public class Carrinho
 {
+    /**
+     * Inicializa o ArrayList<Cliente> na classe
+     */
     private ArrayList<Cliente> clienteArrayList = new ArrayList<Cliente>();
 
+    /**
+     * Função para adicionar um novo cliente no carrinho
+     * 
+     * @param cliente Cliente Objeto cliente
+     */
     public void addCliente( Cliente cliente )
     {
         clienteArrayList.add( cliente );
     }
 
+    /**
+     * Função para retornar o arry com todos os clientes adicionados no carrinho
+     * 
+     * @return ArrayList<Cliente>
+     */
     public ArrayList<Cliente> getCompras()
     {
         return clienteArrayList;

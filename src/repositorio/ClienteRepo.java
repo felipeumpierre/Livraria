9eb39ea054cad003b83a6542ab568bdd44a52218
Felipe Pieretti Umpierre <felipeumpierre@hotmail.com>
2015-03-21 10:45:25 -0300

    Update ClienteRepo.java
    Adicionado os comentários e javadoc

--- src/repositorio/ClienteRepo.java
+++ src/repositorio/ClienteRepo.java
@@ -5,23 +5,44 @@ import java.util.ArrayList;
 
 public class ClienteRepo
 {
+    /**
+     * Class var from type ArrayList<Livro>
+     */
     private ArrayList<Livro> livroArrayList;
 
+    /**
+     * Constructor that only create a new object in the var
+     */
     public ClienteRepo()
     {
         livroArrayList = new ArrayList<Livro>();
     }
 
+    /**
+     * Method that add a new Livro in the Client list
+     * 
+     * @param livro Livro Livro object
+     */
     public void addLivro( Livro livro )
     {
         livroArrayList.add( livro );
     }
 
+    /**
+     * Method that remove a livro from a index
+     * 
+     * @param index int number of the index
+     */
     public void removeLivro( int index )
     {
         livroArrayList.remove( index );
     }
 
+    /**
+     * Method that return the full ArrayList<Livro>
+     * 
+     * @return ArrayList<Livro>
+     */
     public ArrayList<Livro> getLivros()
     {
         return livroArrayList;
